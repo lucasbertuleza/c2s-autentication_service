333a1a7bbc461e304e178693d89b965f83fc91aa
Lucas Bertuleza <lucascbertuleza@gmail.com>
2024-11-07 17:51:11 +0000

    feat: criar modelo User

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  validates :email, uniqueness: true, presence: true, format: {with: URI::MailTo::EMAIL_REGEXP}
+  validates :password_digest, presence: true
+  has_secure_password
+
+  def as_json
+    super.except("password_digest")
+  end
+end
